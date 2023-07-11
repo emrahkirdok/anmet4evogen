01f0bb11954f44344301c89046cbd7d4e5ee11a2
Emrah Kırdök <emrahkirdok@gmail.com>
2023-07-11 11:39:24 +0300

    download scripts

--- scripts/download-jensen.sh
+++ scripts/download-jensen.sh
@@ -1,7 +1,7 @@
 
 # download one file from jensen et al 2019
+
+mkdir -p proje-1/data/fastq; cd proje-1/data/fastq
+ 
 wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR325/004/ERR3250144/ERR3250144.fastq.gz
 
-mkdir -p data
-
-mv ERR3250144.fastq.gz data
