008f56579ba4b018b4748dc2726531fb67673ac1
Egitim Kullanicisi <egitim@levrek1.ib>
2023-07-11 11:37:58 +0300

    kraken2 download script

---
+++ scripts/download-kraken2db.sh
@@ -0,0 +1,7 @@
+
+mkdir -p proje-1/data/databases/kraken2; cd proje-1/data/databases/kraken2
+ 
+wget https://genome-idx.s3.amazonaws.com/kraken/k2_standard_08gb_20230605.tar.gz
+
+tar -xzf k2_standard_08gb_20230605.tar.gz
+
